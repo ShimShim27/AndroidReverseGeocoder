8b33eb5d7efe389cbd8c17a22776c39577120207
ShimShim27 <84265457+ShimShim27@users.noreply.github.com>
2022-04-11 19:39:39 +0800

    Added sample code usage

--- app/src/main/java/shim/shim/reversegeocoderapp/MainActivity.kt
+++ app/src/main/java/shim/shim/reversegeocoderapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package shim.shim.reversegeocoderapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import shim.shim.androidreversegeocoder.ReverseGeocoder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val reverseGeocoder = ReverseGeocoder(nThreads = 1, nCoordinatesCached = 50)
+        reverseGeocoder.reverse(
+            context = this,
+            lat = 10.68532,
+            long = -146.69885,
+            onAddressResult = {addressName:String->
+                runOnUiThread {
+                    Toast.makeText(this,"The address is $addressName", Toast.LENGTH_SHORT).show()
+                }
+            })
+
     }
 }
